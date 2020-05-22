ca9e0b45f65d1ff2e484e7145c59efd841ae63fd
John15321 <johngadsden1775@gmail.com>
2020-05-22 14:48:56 +0200

    .

--- CW/CW7/test/main.py
+++ CW/CW7/test/main.py
@@ -2,16 +2,22 @@ import os
 
 num = [1]
 den = [1, 2, 3, 4]
-plot_name = "JEBAC_POLICJE.png"
+plot_name = "linuxtest.png"
 
 
 if os.name == "nt":
     print("Detected OS: Windows")
-    command = "matlab -nodesktop -r \"testowa("+str(repr(num))+","+str(repr(den))+","+ "\'"+plot_name+"\');exit;\""
-    os.system(command)
+    command = "matlab -nodesktop -r \"testowa("+str(repr(num))+","+str(
+        repr(den))+"," + "\'"+plot_name+"\');exit;\""
     print("Command/s to be run: \n"+command)
+    os.system(command)
 elif os.name == "posix":
     print("Detected OS: Linux")
+    #  -nodesktop -r "testowa([1],[1, 2, 3, 4],'JEBAC_POLICJE.png');exit;"
+    command = "/usr/local/MATLAB/R2018a/bin/matlab -softwareopengl -nodesktop -r \"testowa("+str(
+        repr(num))+","+str(repr(den))+"," + "\'"+plot_name+"\');exit;\""
+    print("Command/s to be run: \n"+command)
+    os.system(command)
 else:
     print("Error: Deteced OS: Not known!")
     raise SystemExit
