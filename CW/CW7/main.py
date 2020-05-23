a661ee8d0490be7170afecbfd130428155057423
John15321 <56229312+John15321@users.noreply.github.com>
2020-05-23 20:09:55 +0200

    .

--- CW/CW7/main.py
+++ CW/CW7/main.py
@@ -37,8 +37,8 @@ K_d = 1/(s**2+4*s+5)
 
 LATEX_K_LIST = [str(sp.latex(K_a)), str(sp.latex(K_b)),
                 str(sp.latex(K_c)), str(sp.latex(K_d))]
-K_LIST = [(K_a),(K_b),
-          (K_c),(K_d)]
+K_LIST = [(K_a), (K_b),
+          (K_c), (K_d)]
 G_a = k
 G_b = 1/((s+1)*(s+2))
 G_c = 1/(s+3)
@@ -56,8 +56,8 @@ L_K_b, M_K_b = sp.fraction(K_b)
 L_K_c, M_K_c = sp.fraction(K_c)
 L_K_d, M_K_d = sp.fraction(K_d)
 
-K_L_LIST=[]
-K_M_LIST=[]
+K_L_LIST = []
+K_M_LIST = []
 for x in K_LIST:
     K_L_LIST.append(sp.fraction(x)[0])
     K_M_LIST.append(sp.fraction(x)[1])
@@ -67,8 +67,8 @@ L_G_b, M_G_b = sp.fraction(G_b)
 L_G_c, M_G_c = sp.fraction(G_c)
 L_G_d, M_G_d = sp.fraction(G_d)
 
-G_L_LIST=[]
-G_M_LIST=[]
+G_L_LIST = []
+G_M_LIST = []
 for x in G_LIST:
     G_L_LIST.append(sp.fraction(x)[0])
     G_M_LIST.append(sp.fraction(x)[1])
@@ -77,17 +77,23 @@ for x in G_LIST:
 LATEX_K_Z_LIST = [str(sp.latex((L_K_a*M_G_a)/(L_K_a*L_G_a+M_K_a*M_G_a))), str(sp.latex((L_K_b*M_G_b/(L_K_b*L_G_b+M_K_b*M_G_b)))),
                   str(sp.latex((L_K_c*M_G_c)/(L_K_c*L_G_c+M_K_c*M_G_c))), str(sp.latex((L_K_d*M_G_d/(L_K_d*L_G_d+M_K_d*M_G_d))))]
 # Kazde K_Z jako expr
-K_Z_LIST=[]
+K_Z_LIST = []
 for x in range(4):
     K_Z_LIST.append((K_L_LIST[x]*G_M_LIST[x])/(K_L_LIST[x]*G_L_LIST[x]+K_M_LIST[x]*G_M_LIST[x]))
+# Dzieli K_Z na mianownik i licznik
+K_Z_L_LIST=[]
+K_Z_M_LIST=[]
+for x in K_Z_LIST:
+    K_Z_L_LIST.append(sp.fraction(x)[0])
+    K_Z_M_LIST.append(sp.fraction(x)[1])
 # Hurwitz, dla kazdego przykladu, dla petli zamknietej
 # Dostajemy booleana czy jest stabilny czy nie
 # oraz wersje latexowa(string) K_z
-K_Z_H_MATRIX=[]
-K_Z_H_DETS=[]
+K_Z_H_MATRIX = []
+K_Z_H_DETS = []
 for x in K_Z_LIST:
     K_Z_H_MATRIX.append(hr.Hurwitz_sp(x)[0])
-    K_Z_H_DETS.append(hr.Hurwitz_sp(x))
+    K_Z_H_DETS.append(hr.Hurwitz_sp(x)[1])
 # Licznik i Mianownik K'atych (jako listy wspolczynnikow przy s'ach)
 L_KG_LIST_a, M_KG_LIST_a = [[float(i) for i in sp.Poly(
     i, s).all_coeffs()] for i in (K_a*G_a).replace(k, 1).as_numer_denom()]
@@ -139,8 +145,8 @@ os.system(COMMAND_START)
 # print("Waiting for Matlab to finish:")
 i = 0
 while not os.path.exists("end_of_mat.txt"):
-    if i == 1: # to sie i tak nie wykona
-        print("wtf") 
+    if i == 1:  # to sie i tak nie wykona
+        print("wtf")
 
 # print("Matlab has finished", end="")
 
@@ -153,7 +159,19 @@ STABS_1_KG = df[1].to_list()
 # print("-"*50)
 if os.path.exists("stabs.txt"):
     os.remove("stabs.txt")
-if os.path.exists("end_of_mat.txt"):
-    os.remove("end_of_mat.txt")
+# if os.path.exists("end_of_mat.txt"):
+#     os.remove("end_of_mat.txt")
 
-#DO LATEXA !!!!!!!!!!!!!!
+# DO LATEXA !!!!!!!!!!!!!!
+for x in range(1):
+    print("\\textbf{Przykład }\n$$K="+sp.latex(K_LIST[x])+"$$\n"+"$$G="+sp.latex(G_LIST[x])+"$$")
+    print("Transmitancja CLS:\n$$K_Z=\\frac{k}{1+KG}=\\frac{" + sp.latex(K_LIST[x]) + "}{1+" + sp.latex(K_LIST[x]) + sp.latex(G_LIST[x]) + "}=" +sp.latex(K_Z_LIST[x])+"$$")
+    print("Transmitancja OLS:\n$$K_O=K\\cdot G="+sp.latex(K_LIST[x]*G_LIST[x])+"$$")
+    print("\\textbf{Sprawdzamy stabilność CLS z pomocą kryterium Hurwitza:}\\newline")
+    print("Wielomian charakterystyczny CLS:\n$$M_Z=" + sp.latex(K_Z_M_LIST[x]) + "$$")
+    print("Macierz Hurwitza na podstawie tego wielomianu oraz wartości wyznacznika i podwyznaczników:")
+    print("$$" + Hurwitz_sp(K_Z_LIST(x))[0], "$$\n Tabela z wyznacznikami\n$$" + Hurwitz_sp(K_Z_LIST(x))[1], "$$")
+    print("%oceniam stabilnosc recznie")
+    print("\\newline\\textbf{Sprawdzam stabilność CLS z pomocą kryterium Nyquista}\\newline")
+    print("Wykres Nyquista dla 1+KG:\n\\includegraphics[width=12cm]{}")
+    print("%oceniam stabilnosc recznie")
